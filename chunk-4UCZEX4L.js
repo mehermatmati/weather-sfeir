0b8bf4633c592bebb16fd26d9c3b985493d7dd66
Maher MATMATI <meher.matmati@gmail.com>
2024-05-05 22:16:41 +0200

    Auto-generated commit

---
+++ chunk-4UCZEX4L.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-MLKXI5BN.js";import"./chunk-EZ25GSZQ.js";export{a as CurrentWeatherModule};
---
+++ chunk-OJ2SYR3G.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-2GB6WDZQ.js";import"./chunk-EZ25GSZQ.js";export{a as ForecastsModule};
